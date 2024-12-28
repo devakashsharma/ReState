5f7917c0eef302dd02b10d74c9b037680cf97bdf
Akash Sharma <soulakash20002@gmail.com>
2024-12-28 23:30:14 +0530

    Login

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
-import { Account, Avatars, Client } from "react-native-appwrite";
+import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
 import * as Linking from "expo-linking";
+import { startAsync as openAuthSessionAsync } from 'expo-auth-session';
+
 
 export const config = {
     platform: "com.shinobi.restate",
@@ -19,7 +21,28 @@ export const account = new Account(client);
 
 export async function login() {
     try {
-        //
+        const redirectURL = Linking.createURL("/");
+
+        const response = await account.createOAuth2Token(OAuthProvider.Google, redirectURL)
+
+        if (!response) throw new Error("Failed to login");
+
+        const browserResult = await openAuthSessionAsync(response.toString(), redirectURL);
+
+        if (browserResult.type !== "success") throw new Error("Failed to login");
+
+        const url = new URL(browserResult.url);
+
+        const secret = url.searchParams.get("secret")?.toString();
+        const userID = url.searchParams.get("user")?.toString();
+
+        if (!secret || !userID) throw new Error("Failed to login");
+
+        const session = await account.createSession(userID, secret);
+
+        if (!session) throw new Error("Failed to login");
+
+        return true;
     } catch (error) {
         console.error(error);
         return false;

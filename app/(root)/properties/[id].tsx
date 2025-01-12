f6a3c3da6d64135e490939276765640e0218b1dd
Akash Sharma <soulakash20002@gmail.com>
2025-01-12 20:48:43 +0530

    Gallery

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import { View, Text, ScrollView, Image, TouchableOpacity, FlatList } from "react-native";
 import React from "react";
 import { router, useLocalSearchParams } from "expo-router";
 import { Dimensions } from "react-native";
@@ -119,7 +119,27 @@ const Property = () => {
         </View>
       </View>
 
-      
+      {property?.gallery.length > 0 && (
+            <View className="m-7">
+              <Text className="text-black-300 text-xl font-rubik-bold">
+                Gallery
+              </Text>
+              <FlatList
+                contentContainerStyle={{ paddingRight: 20 }}
+                data={property?.gallery}
+                keyExtractor={(item) => item.$id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item }) => (
+                  <Image
+                    source={{ uri: item.image }}
+                    className="size-40 rounded-xl"
+                  />
+                )}
+                contentContainerClassName="flex gap-4 mt-3"
+              />
+            </View>
+          )}
     </ScrollView>
   );
 };

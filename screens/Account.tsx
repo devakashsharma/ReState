8cd61d3c030db67606134750c483a89fa2a4cba9
Akash Sharma <soulakash20002@gmail.com>
2025-01-16 23:41:45 +0530

    Account update

--- screens/Account.tsx
+++ screens/Account.tsx
@@ -7,14 +7,19 @@ import { useGlobalContext } from "@/lib/global-provider";
 export default function Account() {
     const { user, refetch } = useGlobalContext();
   return (
-    <SafeAreaView className='m-7'>
-        <View>
-            <Text>Profile</Text>
+    <SafeAreaView className='m-7 w-full h-full flex flex-col justify-center'>
+        <View className='flex flex-col justify-center items-center'>
+            <Text className='text-3xl font-rubik-bold'>Profile</Text>
             <View>
-                <Image source={{uri: user?.avatar}} />
+                <Image source={{uri: user?.avatar}} className='size-44 rounded-full' />
             </View>
         </View>
-      <Text>Account</Text>
+      <View className='flex flex-col justify-start items-start gap-3'>
+        <View className='flex flex-col'>
+          <Text className='text-lg font-rubik'>Name</Text>
+          <Text className='text-xl font-rubik-bold'>{user?.name}</Text>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
